a46bd7df0c03b70de279312f49927c46ca76d906
Marcelofury <marcelofury13579@gmail.com>
2026-02-24 13:21:46 +0300

    imported main.vue component

--- vuePractice/vuePractice/src/App.vue
+++ vuePractice/vuePractice/src/App.vue
@@ -1,19 +1,18 @@
 <script setup>
 import {ref} from 'vue'
 import Header from '@/Header.vue'
-const name = ref('Racheal')
+import Main from '@/main.vue'
+
 
 const year = ref('2026')
 </script>
 
 <template>
   <Header/>
+  <Main/>
  
 
-  <main>
-    <h1> Hello, {{ name }} !</h1>
-    <p>I'm about to learn vue! </p>
-  </main>
+  
   <footer>
     <p>&copy; {{ year }}</p>
   </footer>
@@ -21,9 +20,7 @@ const year = ref('2026')
 
 <style scoped>
 
-main h1{
-  display: block;
-}
+
 footer {
   position: fixed;
 bottom: 0;
