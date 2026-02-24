c6137a067d6145e2e186cb7da817d81764c9d419
Marcelofury <marcelofury13579@gmail.com>
2026-02-24 13:04:35 +0300

    initial commit of vuePractice

---
+++ vuePractice/vuePractice/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import {ref} from 'vue'
+const name = ref('Racheal')
+const emoji = ref('✌️')
+const year = ref('2026')
+</script>
+
+<template>
+  <header>
+    <span>{{ emoji }}</span>
+  </header>
+
+  <main>
+    <h1> Hello, {{ name }} !</h1>
+    <p>I'm about to learn vue! </p>
+  </main>
+  <footer>
+    <p>&copy; {{ year }}</p>
+  </footer>
+</template>
+
+<style scoped>
+span{
+  font-size: 3rem;
+
+}
+main h1{
+  display: block;
+}
+footer {
+  position: fixed;
+bottom: 0;
+padding-bottom: 20px;
+}
+footer p {
+  color: blue;
+
+}
+
+</style>
