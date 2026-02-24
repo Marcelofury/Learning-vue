2ae34e90b66e4bde7fbfd52566f16e92d3aef6a9
Marcelofury <marcelofury13579@gmail.com>
2026-02-24 13:25:42 +0300

    imported footer.vue component

--- vuePractice/vuePractice/src/App.vue
+++ vuePractice/vuePractice/src/App.vue
@@ -4,31 +4,19 @@ import Header from '@/Header.vue'
 import Main from '@/main.vue'
 
 
-const year = ref('2026')
+
 </script>
 
 <template>
   <Header/>
   <Main/>
+  <Footer/>
  
 
   
-  <footer>
-    <p>&copy; {{ year }}</p>
-  </footer>
 </template>
 
 <style scoped>
 
-
-footer {
-  position: fixed;
-bottom: 0;
-padding-bottom: 20px;
-}
-footer p {
-  color: blue;
-
-}
 
 </style>
