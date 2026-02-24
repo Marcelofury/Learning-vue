07fc908018db903eaa3767054917b1b21a5f98ce
Marcelofury <marcelofury13579@gmail.com>
2026-02-24 13:21:15 +0300

    created main.vue componet

---
+++ vuePractice/vuePractice/src/main.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from 'vue'
+const name = ref('Racheal')
+</script>
+
+<template>
+<main>
+    <h1> Hello, {{ name }} !</h1>
+    <p>I'm about to learn vue! </p>
+  </main>
+</template>
+
+<style scoped>
+main h1{
+  display: block;
+}
+</style>
